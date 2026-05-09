a44693f7a6942365a80fdf003f068afe0b9fea33
18-05-Amankumarverma <akverma2017.jsr@gmail.com>
2026-05-09 21:21:25 +0530

    added file storage for data

--- Login.java
+++ Login.java
@@ -1,6 +1,9 @@
 import login.InputSanitizer;
 import login.status;
 import java.time.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
 
 interface auth{
     public String createAccount(String username,String email,String password);
@@ -25,28 +28,89 @@ public class Login implements auth{
         this.email = obj.sanitize(email);
         this.password = obj.sanitize(password);
 
-        return status.SUCCESS_ACC.getDescription();
+        LocalDateTime td =  LocalDateTime.now();
+
+        try{
+            FileWriter fw = new FileWriter("db_auth.txt",true);
+            fw.write("\n"+td+","+this.username +","+ this.email +","+this.password );
+            fw.close();
+            return status.SUCCESS_ACC.getDescription();
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return status.FAILED_ACC.getDescription();
     }
 
-    public String login(String email,String password){
+    public String login(String email,String password) {
         InputSanitizer obj = new InputSanitizer();
 
         String sanitizeEmail = obj.sanitize(email);
         String sanitizePassword = obj.sanitize(password);
 
-        if(sanitizeEmail.equals(this.email) && sanitizePassword.equals(this.password)){
-            return status.SUCCESS_LOG.getDescription();
-        }
-        else{
+       try{
+           File fs = new File("db_auth.txt");
+           Scanner sc = new Scanner(fs);
+           while (sc.hasNextLine()) {
+               String data = sc.nextLine();
+               String[] splittedData = data.split(",");
+               for(String value : splittedData){
+                   System.out.println(value);
+               }
+               if(sanitizeEmail.equals(splittedData[2]) && sanitizePassword.equals(splittedData[3])) {
+                   sc.close();
+                   return status.SUCCESS_LOG.getDescription();
+               }
+           }
+       } catch (Exception e) {
+           e.printStackTrace();
+       }
             return status.FAILED_LOG.getDescription();
-        }
     }
+
+
 
     public static void main(String[] args) {
         Login lg = new Login();
-        LocalDateTime td =  LocalDateTime.now();
-        System.out.println(td);
-        System.out.println(lg.createAccount("aman","aman@gmail.com","aman@123"));
-        System.out.println(lg.login("aman@gmail.com","aman@123"));
+        Scanner sc = new Scanner(System.in);
+    Boolean flag = true;
+        while(flag){
+            System.out.println("""
+                    1. Create Account
+                    2. Login
+                    3. Exit
+                    
+                    """);
+            int choice = sc.nextInt();
+            sc.nextLine();
+            switch(choice){
+                case 1:
+                    System.out.println("Enter your Name :");
+                    String name = sc.nextLine();
+                    System.out.println("Enter your email :");
+                    String email = sc.nextLine();
+                    System.out.println("Enter your password :");
+                    String password = sc.nextLine();
+
+                    System.out.println("\n" + lg.createAccount(name,email,password));
+                    break;
+                case 2:
+                    System.out.println("Enter your email : ");
+                    String email_ = sc.nextLine();
+                    System.out.println("Enter your password : ");
+                    String password_ = sc.nextLine();
+                    System.out.println("\n" + lg.login(email_,password_));
+                    break;
+
+                default :
+                    System.out.println("authentication is  stoped ");
+                    flag = false;
+
+            }
+        }
+
+
+
     }
 }
